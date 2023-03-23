f1f07e32fd73e01d749cec162d12678f8c1c0e9f
Denis ibanez Rodrigues vieira <denisibanezrodriguesvieira@MacBook-Pro-de-Denis.local>
2023-03-22 22:55:20 -0300

    feature: insert storybook configs

---
+++ .storybook/theme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Master Components',
+  brandUrl: 'https://git.bancomaxima.net.br/corporate/masterlab/front-end/design-system-ui',
+  brandImage: 'https://www.bancomaster.com.br/images/ib-logo.png',
+});
