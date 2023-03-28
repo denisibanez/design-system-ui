ce21d63bbff0dacb6b3e9c601cef601158fe0f4d
Denis ibanez Rodrigues vieira <denisibanezrodriguesvieira@MacBook-Pro-de-Denis.local>
2023-03-28 13:42:42 -0300

    feature

--- src/components/layout/QcLayout.stories.js
+++ src/components/layout/QcLayout.stories.js
@@ -1,9 +1,9 @@
-import Qclayout from './Qclayout.vue';
+import QcLayout from './QcLayout.vue';
 
 // More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
 export default {
   title: 'Components/layout',
-  component: Qclayout,
+  component: QcLayout,
   // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
   argTypes: { onClick: { action: 'clicked' } },
 };
@@ -11,13 +11,13 @@ export default {
 // More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
 const Template = (args) => ({
   // Components used in your story `template` are defined in the `components` object
-  components: { Qclayout },
+  components: { QcLayout },
   // The story's `args` need to be mapped into the template through the `setup()` method
   setup() {
     return { args };
   },
   // And then the `args` are bound to your component with `v-bind="args"`
-  template: '<Qclayout v-bind="args" />',
+  template: '<QcLayout v-bind="args" @navigate="onClick" @logoff="onClick" />',
 });
 
 export const Primary = Template.bind({});
