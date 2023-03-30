1e903f066bf2ca08cad79b04c1828fd12ab21377
Denis ibanez Rodrigues vieira <denisibanezrodriguesvieira@MacBook-Pro-de-Denis.local>
2023-03-30 12:12:40 -0300

    feature:change infos

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -3,6 +3,6 @@ import { create } from '@storybook/theming';
 export default create({
   base: 'dark',
   brandTitle: 'Design System Components',
-  brandUrl: 'https://github.com/denisibaneza/design-system-ui',
+  brandUrl: 'https://github.com/denisibanez/design-system-ui',
   brandImage: 'https://i.ytimg.com/vi/qPkSwo8QyoA/maxresdefault.jpg',
 });
